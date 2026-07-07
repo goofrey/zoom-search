5d03f01079b542de8a5580bfac4aed6909e10759
goofrey <64430698+goofrey@users.noreply.github.com>
2026-07-07 06:50:49 +0000

    chore: bump version to 0.1.3

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,5 +2,5 @@ import zoom_search
 
 
 def test_import_zoom_search() -> None:
-    assert zoom_search.__version__ == "0.1.2"
+    assert zoom_search.__version__ == "0.1.3"
     assert hasattr(zoom_search, "SearchRequest")
